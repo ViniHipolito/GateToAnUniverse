1e65e333a898dacba9994647491c38953980416c
ViniHipolito <97119997+ViniHipolito@users.noreply.github.com>
2025-12-02 17:56:08 -0300

    Update debugger.py

--- debugger.py
+++ debugger.py
@@ -3,10 +3,10 @@ import time
 theeList = ["???: Outro video jogo, não?", "???: Vós conhece o primeiro.", "???: Não é um erro acidental esta linha de código.",
 "???: Vós tem um raciocínio... magnifico", "???: Quem eres eu?", "???: Um Humano, apenas.", 
 "???: Este Bispo? Não há tempo de tela o bastante.", '???: Atrás de um simples jogo de "Enigma".',
-"???: Odeio ter de abrir os olhos de uns.","???: Mas... a Sheller está esperando vós.",
-"Zacharie: Também estarei."]
+"???: Odeio ter de abrir os olhos de uns.","???: ...",
+"???: O que é isso?", '???: Bem, um pequeno "Easter Egg".', "???: Enfim... a Sheller está esperando vós.", "Zacharie: Também estarei"]
 
 def DebuggingProcess():
   for x in theeList:
     print(x)
-    time.sleep(1)
+    time.sleep(2)
